fdffd8a137643437d1c91c81aa9788e9bb054a2d
edpau <edpau@me.com>
2024-11-29 10:31:08 +0000

    feat: add Tailwind CSS styling

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,9 +1,11 @@
 function AddTodo({onTodoSubmit}) {
-    return <section>
+    return <section className="relative">
       <label className="sr-only" htmlFor="addTodo">
         Add Todo
       </label>
-      <input id="addTodo" placeholder="Create a new todo..." onKeyDown={onTodoSubmit}></input>
+      <span className="w-5 h-5 left-4 rounded-full absolute top-1/2 translate-y-[35%] border border-check-border"></span>
+      <input className="w-full rounded-md pl-12 py-4 mt-9 text-xs text-input-text bg-input-bg" id="addTodo" placeholder="Create a new todo..." onKeyDown={onTodoSubmit}/>
+      
     </section>;
   }
   
