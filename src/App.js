cd6dace7c8b83bfe2b0e1a9900eaeae0826778b0
edpau <edpau@me.com>
2024-11-29 10:29:19 +0000

    feat: add Tailwind CSS styling and add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Todo from "./components/Todo/Todo";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-hero-mobile--light dark:bg-hero-mobile--dark min-h-screen bg-contain bg-no-repeat px-6 pt-12 bg-background">
+      <Header title={"Todo"} />
       <Todo />
     </div>
   );
