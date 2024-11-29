1990c5a06d56394eeee353e5feaaedebc2d47cff
edpau <edpau@me.com>
2024-11-29 10:40:00 +0000

    feat: add Tailwind CSS styling, seperate radio button to its own div

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -1,27 +1,81 @@
-function TodoFooter({onFilterChange, onClearComplete, numOfActive}) {
-
-    return (
-      <section>
-        <fieldset>
-          <legend>filter todo list</legend>
-          <label>
-            <input type="radio" name="filter" value="all" defaultChecked={true}  onChange={onFilterChange}/>
-            All
-          </label>
-          <label>
-            <input type="radio" name="filter" value="active" onChange={onFilterChange}/>
-            Active
-          </label>
-          <label>
-            <input type="radio" name="filter" value="completed" onChange={onFilterChange}/>
-            Completed
-          </label>
+function TodoFooter({ onFilterChange, onClearComplete, numOfActive }) {
+  return (
+    <>
+      <section className="text-footer-text grid grid-cols-2 gap-y-4">
+        <p
+          className="order-1 rounded-bl-[0.3125rem] bg-todo-bg pb-[1.375rem] pl-5 pt-4 text-xs"
+          aria-live="polite"
+        >
+          {numOfActive < 2
+            ? `${numOfActive} item left`
+            : `${numOfActive} items left`}
+        </p>
+        <fieldset className="order-2 col-span-2 flex justify-center gap-3 rounded-[0.3125rem] bg-todo-bg py-[0.9375rem]">
+          <legend className="sr-only">filter todo list</legend>
+          <div>
+            <input
+              className="peer sr-only"
+              type="radio"
+              name="filter"
+              value="all"
+              id="all"
+              defaultChecked={true}
+              onChange={onFilterChange}
+            />
+            <label
+              className="peer-checked:text-footer-text-hover cursor-pointer"
+              htmlFor="all"
+            >
+              {" "}
+              All
+            </label>
+          </div>
+          <div>
+            <input
+              className="peer sr-only"
+              type="radio"
+              name="filter"
+              value="active"
+              id="active"
+              onChange={onFilterChange}
+            />
+            <label
+              className="peer-checked:text-footer-text-hover cursor-pointer"
+              htmlFor="active"
+            >
+              {" "}
+              Active
+            </label>
+          </div>
+          <div>
+            <input
+              className="peer sr-only"
+              type="radio"
+              name="filter"
+              id="filter"
+              value="completed"
+              onChange={onFilterChange}
+            />
+            <label
+              className="peer-checked:text-footer-text-hover cursor-pointer"
+              htmlFor="filter"
+            >
+              Completed
+            </label>
+          </div>
         </fieldset>
-        <p>{(numOfActive<2) ? `${numOfActive} item left` : `${numOfActive} items left`}</p>
-        <button onClick={onClearComplete}>Clear Completed</button>
+        <button
+          className="order-1 rounded-br-[0.3125rem] bg-todo-bg pr-5 text-right text-xs"
+          onClick={onClearComplete}
+        >
+          Clear Completed
+        </button>
       </section>
-    );
-  }
-  
-  export default TodoFooter;
-  
+      <p className="text-footer-text w-full pt-10 text-center text-[0.875rem]">
+        Drag and drop to reorder list
+      </p>
+    </>
+  );
+}
+
+export default TodoFooter;
