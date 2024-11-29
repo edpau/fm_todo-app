c81a21395b83b7281ef6352505aa4a6c2254e472
edpau <edpau@me.com>
2024-11-29 10:46:19 +0000

    style: extend Tailwind CSS configuration with custom fonts, background images, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "josefin-sans": ["Josefin Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-mobile--light": "url('/src/assets/images/bg-mobile-light.jpg')",
+        "hero-mobile--dark": "url('/src/assets/images/bg-mobile-dark.jpg')",
+        "hero-desktop--light": "url('/src/assets/images/bg-desktop-light.jpg')",
+        "hero-desktop--dark": "url('/src/assets/images/bg-desktop-dark.jpg')",
+        "check-gradient":
+          "linear-gradient(140deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+      },
+      colors: {
+        background: "hsla(var(--background))",
+        heading: "hsla(var(--heading))",
+        "input-text": "hsla(var(--input-text))",
+        "input-bg": "hsla(var(--input-bg))",
+        "check-border": "hsla(var(--check-border))",
+        "check-border-hover":
+          "linear-gradient(140deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+        "todo-bg": "hsla(var(--todo-bg))",
+        "todo-border": "hsla(var(--todo-border))",
+        "todo-text": "hsla(var(--todo-text))",
+        "todo-text-complete": "hsla(var(--todo-text-complete))",
+        "footer-text": "hsla(var(--footer-text))",
+        "footer-text-hover": "hsla(var(--footer-text-hover))",
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
