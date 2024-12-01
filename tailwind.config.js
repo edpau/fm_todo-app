b65fb4ad46d168acd96d2e791004558558fd362e
edpau <edpau@me.com>
2024-12-01 18:05:53 +0000

    style: extend Tailwind CSS configuration with custom background size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundSize: {
+        "bgSize": "100% 30vh"
+      },
       fontFamily: {
         "josefin-sans": ["Josefin Sans", "sans-serif"],
       },
