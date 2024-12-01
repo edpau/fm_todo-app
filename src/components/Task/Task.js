8949aa0a3001cd5e63758b6a4655ddd0566bd787
edpau <edpau@me.com>
2024-12-01 11:56:50 +0000

    feat: add drag and drop and extract Task component from TodoList

---
+++ src/components/Task/Task.js
@@ -0,0 +1,74 @@
+import checkIcon from "../../assets/images/icon-check.svg";
+import crossIcon from "../../assets/images/icon-cross.svg";
+
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export default function Task({
+  id,
+  task,
+  complete,
+  onToggleTodoCompletion,
+  onClickDeleteTodo,
+}) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  return (
+    <li
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      key={id}
+      className={`group flex touch-none justify-between border-b border-todo-border px-5 py-4 cursor-move`}
+      aria-label={complete ? `${task}, completed` : task}
+    >
+      <button
+        className="group relative flex"
+        onClick={() => {
+          onToggleTodoCompletion(id);
+        }}
+        aria-label="toggle todo completion"
+      >
+        <span className="h-5 w-5 rounded-full bg-check-border group-hover:bg-check-gradient"></span>
+        <span
+          className={`absolute left-[0.05rem] top-[0.04rem] h-[1.15rem] w-[1.15rem] rounded-full ${complete ? "bg-check-gradient" : "bg-todo-bg"}`}
+        ></span>
+        <span className="absolute left-1 top-[0.3rem]">
+          {complete && <img src={checkIcon} alt="check" />}
+        </span>
+        <span
+          className={`${complete === true && "text-todo-text-complete line-through"} ml-3 text-xs text-todo-text`}
+        >
+          {task}
+        </span>
+        <span aria-live="polite" className="sr-only">
+          {" "}
+          {complete ? "completed" : "not completed"}
+        </span>
+      </button>
+
+      <button
+        onClick={() => {
+          onClickDeleteTodo(id);
+        }}
+        aria-label="delete todo"
+        className="opacity-0 group-hover:opacity-100"
+      >
+           <svg className="text-[#494C6B] hover:text-cross-hover transition-colors" xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 18 18">
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+          />
+        </svg>
+      </button>
+    </li>
+  );
+}
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,44 +1,16 @@
-import checkIcon from "../../assets/images/icon-check.svg";
-import crossIcon from "../../assets/images/icon-cross.svg"
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+
+import Task from "../Task/Task";
 
 function TodoList({ filteredTodos = [], onToggleTodoCompletion, onClickDeleteTodo }) {
   return (
     <section>
       <ul className="bg-todo-bg mt-4 w-full rounded-t-[5px]">
+        <SortableContext items={filteredTodos} strategy={verticalListSortingStrategy}>
         {filteredTodos.map((todo) => (
-          <li
-            key={todo.id}
-            className={`border-todo-border flex justify-between border-b px-5 py-4`}
-            aria-label={todo.complete ? `${todo.task}, completed` : todo.task}
-          >
-            <button
-              className="relative flex group"
-              onClick={() => {
-                onToggleTodoCompletion(todo.id);
-              }}
-              aria-label="toggle todo completion"
-            >
-              <span className=" h-5 w-5 rounded-full bg-check-border group-hover:bg-check-gradient"></span>
-              <span className={`absolute top-[0.04rem] left-[0.05rem] h-[1.15rem] w-[1.15rem] rounded-full  ${todo.complete ? "bg-check-gradient" : "bg-todo-bg"}`}></span>
-              <span className="absolute top-[0.3rem] left-1">{todo.complete && <img src={checkIcon} alt="check" />}</span>
-              <span
-                className={`${todo.complete === true && "line-through text-todo-text-complete"} ml-3 text-xs text-todo-text`}
-              >
-                {todo.task}
-              </span>
-              <span aria-live="polite" className="sr-only">  {todo.complete ? 'completed' : 'not completed'}</span>
-            </button>
-
-            <button
-              onClick={() => {
-                onClickDeleteTodo(todo.id)
-              }}
-              aria-label="delete todo"
-            >
-              <img src={crossIcon} alt="cross" className="w-3"/>
-            </button>
-          </li>
+          <Task key={todo.id} id={todo.id} task={todo.task} complete={todo.complete} onToggleTodoCompletion={onToggleTodoCompletion} onClickDeleteTodo={onClickDeleteTodo}/>
         ))}
+      </SortableContext>
       </ul>
     </section>
   );
